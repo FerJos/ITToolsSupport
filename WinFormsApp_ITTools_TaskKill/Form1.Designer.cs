c114134fcee9ae422d5900d89f622f3586062515
FerJAle <fj.alegria@hotmail.com>
2024-03-21 11:18:24 -0600

    Nuevo proyecto.

---
+++ WinFormsApp_ITTools_TaskKill/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WinFormsApp_ITTools_TaskKill
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(191, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Terminar forzosamente una tarea y\r\nprocesos secundarios iniciados por\r\nla tarea especificada.";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(12, 57);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 103);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Tarea";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(94, 74);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(6, 47);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(39, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "IM";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(6, 22);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(43, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "PID";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 176);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Ver lista";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Enabled = false;
+            button2.Location = new Point(147, 176);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Finalizar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(234, 211);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "IT Tools TaskKill";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ WinFormsApp_ITTools_TaskKill/Form1.cs
@@ -0,0 +1,147 @@
+using System.Diagnostics;
+
+namespace WinFormsApp_ITTools_TaskKill
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Process newProcess(string cmdLine)
+        {
+            Process proceso = new Process();
+            proceso.StartInfo.FileName = "cmd.exe";
+            proceso.StartInfo.Arguments = "/c" + cmdLine;
+            proceso.StartInfo.UseShellExecute = true;
+            proceso.StartInfo.CreateNoWindow = false;
+            proceso.StartInfo.WorkingDirectory = Environment.GetEnvironmentVariable("USERPROFILE");
+            return proceso;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Enabled)
+            {
+                Process proceso = newProcess("tasklist.exe");
+                proceso.StartInfo.Arguments = proceso.StartInfo.Arguments + "& pause";
+                try
+                {
+                    proceso.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se pudo iniciar el proceso. " + ex.Message, "Proceso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Enabled)
+            {
+                radioButton1.Enabled = false;
+                radioButton2.Enabled = false;
+                textBox1.Enabled = false;
+                button1.Enabled = false;
+                button2.Enabled = false;
+
+                if (radioButton1.Checked && !radioButton2.Checked)
+                {
+                    int? getPID = null;
+                    try
+                    {
+                        getPID = Convert.ToInt32(textBox1.Text);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("No se pudo iniciar el proceso. " + ex.Message, "Proceso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    
+                    if (getPID != null && getPID >= 0)
+                    {
+                        Process proceso = newProcess("taskkill.exe /F /T /PID " + getPID.ToString());
+                        proceso.StartInfo.UseShellExecute = false;
+                        proceso.StartInfo.CreateNoWindow = true;
+
+                        try
+                        {
+                            proceso.Start();
+                            proceso.WaitForExit();
+                            
+                            switch (proceso.ExitCode)
+                            {
+                                case 0:
+                                    MessageBox.Show("Se finalizó la tarea y sus procesos secundarios correctamente. (Código: 0)", "Proceso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    break;
+                                case 128:
+                                    MessageBox.Show("No se encontró la tarea '" + getPID.ToString() + "'. Haga click en 'Ver lista' para obtener el PID. (Código: 128)", "Proceso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    break;
+                                default:
+                                    MessageBox.Show("Posibles problemas al intentar finalizar la tarea. (Código: " + proceso.ExitCode.ToString() + ")", "Proceso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                    MessageBox.Show(proceso.StandardOutput.ToString(), "Proceso - Standard Output", MessageBoxButtons.OK, MessageBoxIcon.None);
+                                    break;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("No se pudo iniciar el proceso. " + ex.Message, "Proceso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                else if (!radioButton1.Checked && radioButton2.Checked)
+                {
+                    Process proceso = newProcess("taskkill.exe /F /T /IM " + textBox1.Text);
+                    proceso.StartInfo.UseShellExecute = false;
+                    proceso.StartInfo.CreateNoWindow = true;
+
+                    try
+                    {
+                        proceso.Start();
+                        proceso.WaitForExit();
+
+                        switch (proceso.ExitCode)
+                        {
+                            case 0:
+                                MessageBox.Show("Se finalizó la tarea y sus procesos secundarios correctamente. (Código: 0)", "Proceso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                break;
+                            case 128:
+                                MessageBox.Show("No se encontró la tarea '" + textBox1.Text + "'. Haga click en 'Ver lista' para obtener el IM. (Código: 128)", "Proceso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                break;
+                            default:
+                                MessageBox.Show("Posibles problemas al intentar finalizar la tarea. (Código: " + proceso.ExitCode.ToString() + ")", "Proceso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                MessageBox.Show(proceso.StandardOutput.ToString(), "Proceso - Standard Output", MessageBoxButtons.OK, MessageBoxIcon.None);
+                                break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("No se pudo iniciar el proceso. " + ex.Message, "Proceso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                radioButton1.Enabled = true;
+                radioButton2.Enabled = true;
+                textBox1.Enabled = true;
+                textBox1.Text = "";
+                button1.Enabled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = textBox1.Text != "";
+        }
+    }
+}
